6e89bc7a0995fd0cbc562202d5fbebe4f77db8f9
diler546 <makar912@mail.ru>
2023-12-29 11:11:20 +0300

    first commit

---
+++ Сalculator/Program.cs
@@ -0,0 +1,76 @@
+﻿public class MathOperations
+{
+    public double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public double Subtract(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    public double Multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    public double Divide(double num1, double num2)
+    {
+        if (num2 != 0)
+            return num1 / num2;
+        else
+            throw new ArgumentException("Divisor cannot be zero");
+    }
+
+    public double Power(double num1, double num2)
+    {
+        return Math.Pow(num1,num2);
+    }
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Выберите операцию (+, -, *, /, ^):");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        MathOperations mathOperations = new MathOperations();
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = mathOperations.Add(num1, num2);
+                break;
+            case '-':
+                result = mathOperations.Subtract(num1, num2);
+                break;
+            case '*':
+                result = mathOperations.Multiply(num1, num2);
+                break;
+            case '/':
+                try
+                {
+                    result = mathOperations.Divide(num1, num2);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                    return;
+                }
+                break;
+            case '^':
+                result = mathOperations.Power(num1, num2);
+                break;
+            default:
+                Console.WriteLine("Неизвестная операция.");
+                return;
+        }
+
+        Console.WriteLine($"\nРезультат: {result}\n");
+    }
+}
---
+++ СalculatorTests/MathOperationsTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class MathOperationsTests
+    {
+        private MathOperations _mathOperations;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mathOperations = new MathOperations();
+        }
+
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            var result = _mathOperations.Add(1, 2);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Subtract_TwoNumbers_ReturnsDifference()
+        {
+            var result = _mathOperations.Subtract(5, 3);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Multiply_TwoNumbers_ReturnsProduct()
+        {
+            var result = _mathOperations.Multiply(3, 4);
+            Assert.AreEqual(12, result);
+        }
+
+        [TestMethod]
+        public void Divide_TwoNumbers_ReturnsQuotient()
+        {
+            var result = _mathOperations.Divide(10, 2);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Power_TwoNumbers_ReturnsResult()
+        {
+            var result = _mathOperations.Power(2, 3);
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Divide_DivisionByZero_ThrowsException()
+        {
+            _mathOperations.Divide(10, 0);
+        }
+    }
+}
